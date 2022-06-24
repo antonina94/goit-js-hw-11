8faa8f106ab1790213719246f8e2048176661712
Antonina Bondar <tonya.bondar111@gmail.com>
2022-06-24 11:01:25 +0000

    add key

--- src/index.js
+++ src/index.js
@@ -7,73 +7,64 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
+
 const form = document.querySelector('.search-form')
 const gallery = document.querySelector('.gallery')
 const button = document.querySelector('.load-more')
 
-let per_page = 0
+// let per_page = 0
 
 
 const loadmorebtn = new LoadMoreBtn({selector:'[type=button]',
  hidden:true})
-console.log(loadmorebtn)
-
-
 
 const newsPixabayService = new NewsPixabayService()
 console.log(newsPixabayService)
-
-
-
-PIXABAY_KEY = '28190532-07c65b576fc64781f10f94256'
-BASE_URL = 'https://pixabay.com/api/'
 
 
 form.addEventListener('submit', formSubmit)
 loadmorebtn.refs.button.addEventListener('click', onLoadMore)
 
 function formSubmit(event){
-    event.preventDefault()
-    clearGallery()
-    newsPixabayService.query = event.target.elements.searchQuery.value
-    loadmorebtn.show()
-    newsPixabayService.resetPage()
-    if(newsPixabayService.query === ''){
-      loadmorebtn.hide()
-     Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-     return
-    }
-  getPixabay()
-  
+  event.preventDefault()
+  clearGallery()
+  newsPixabayService.query = event.target.elements.searchQuery.value
+  loadmorebtn.show()
+  newsPixabayService.resetPage()
+  if(newsPixabayService.query === ''){
+    loadmorebtn.hide()
+   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+   return
+  }
+getPixabay()
+
 }
 
 async function getPixabay(){
-  loadmorebtn.disable()
-  try {
-      const response = await newsPixabayService.getPixabay()
-      const {hits,totalHits} = response
-
-      console.log(response)
-      loadmorebtn.enable()
-      renderPixabay(hits)
-      if(totalHits < newsPixabayService.page){
-        loadmorebtn.hide()
-        Notify.info("We're sorry, but you've reached the end of search results.")
-      }
-      
-      if(hits.length === 0){
-        loadmorebtn.hide()
-       return Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-      }
-      if(totalHits > newsPixabayService.page){
-        Notify.info(`Hooray! We found ${response.totalHits} images.`)
-      }
-      lightbox()
+loadmorebtn.disable()
+try {
+    const response = await newsPixabayService.getPixabay()
+    console.log(response)
+    loadmorebtn.enable()
+    renderPixabay(response.hits)
+    if(response.totalHits < newsPixabayService.page){
+      loadmorebtn.hide()
+      Notify.info("We're sorry, but you've reached the end of search results.")
+    }
     
-  }
-  catch(error){
-    console.log(error)
-  }
+    if(response.hits.length === 0){
+      loadmorebtn.hide()
+     return Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+    if(response.totalHits > newsPixabayService.page){
+      Notify.info(`Hooray! We found ${response.totalHits} images.`)
+    }
+    lightbox()
+  
+}
+catch(error){
+  console.log(error)
+}
 }
 
 function onLoadMore(){
@@ -84,40 +75,40 @@ getPixabay()
 
 
 
-function renderPixabay(hits){
-  const murkup = hits.map(card =>{
-    return `
-    <div class='photo-card'>
-    <a href='${card.largeImageURL}'><img
-        src='${card.webformatURL}'
-        alt='${card.tags}'
-        loading='lazy'
-      /></a>
-    <div class='info'>
-    <p class='info-item'>
-    <b>likes: ${card.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>views: ${card.views}</b>
-    </p>
-    <p class="info-item">
-      <b>comments: ${card.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>downloads: ${card.downloads}</b>
-    </p>
-    </div>
-    </div>
-    `
-  }).join('')
-  gallery.innerHTML = murkup
+function renderPixabay( hits){
+const murkup = hits.map(card =>{
+  return `
+  <div class='photo-card'>
+  <a href='${card.largeImageURL}'><img
+      src='${card.webformatURL}'
+      alt='${card.tags}'
+      loading='lazy'
+    /></a>
+  <div class='info'>
+  <p class='info-item'>
+  <b>likes: ${card.likes}</b>
+  </p>
+  <p class="info-item">
+    <b>views: ${card.views}</b>
+  </p>
+  <p class="info-item">
+    <b>comments: ${card.comments}</b>
+  </p>
+  <p class="info-item">
+    <b>downloads: ${card.downloads}</b>
+  </p>
+  </div>
+  </div>
+  `
+}).join('')
+gallery.innerHTML = murkup
 }
 
 function clearGallery(){
-  gallery.innerHTML = ''
+gallery.innerHTML = ''
 }
 
 function lightbox(){
-  const lightbox = new SimpleLightbox('.gallery a', {});
+const lightbox = new SimpleLightbox('.gallery a', {});
 }
 
--- src/js/pixabay-service.js
+++ src/js/pixabay-service.js
@@ -1,4 +1,13 @@
-const axios = require('axios');
+PIXABAY_KEY = '28190532-07c65b576fc64781f10f94256'
+BASE_URL = 'https://pixabay.com/api/'
+const options = {
+    headers: {
+      Authorization: PIXABAY_KEY,
+    },
+  };
+
+  const axios = require('axios');
+
 export default class NewsPixabayService {
     constructor(){
         this.searchQuery = ''
@@ -7,27 +16,26 @@ export default class NewsPixabayService {
 
     
     async getPixabay(){
-        try {
-            console.log(this)
-            const url = `${BASE_URL}?key=${PIXABAY_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-            const response = await axios.get(url);
-            const {hits,totalHits} = response.data
-            this.page+=1
-            console.log(response);
-            return {hits,totalHits}
-          } catch (error) {
-            console.error(error);
-          }
-    }
-    resetPage(){
-        this.page = 1
-    }
+      try {
+          const url = `${BASE_URL}?key=${PIXABAY_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+          const response = await axios.get(url);
+          console.log(response);
+          const {hits,totalHits} = response.data
+          this.page+=1
+          return {hits,totalHits}
+        } catch (error) {
+          console.error(error);
+        }
+  }
+  resetPage(){
+      this.page = 1
+  }
 
-    get query(){
-        return this.searchQuery
-    }
+  get query(){
+      return this.searchQuery
+  }
 
-    set query(newQuery){
-        this.searchQuery = newQuery
-    }
+  set query(newQuery){
+      this.searchQuery = newQuery
+  }
 }
