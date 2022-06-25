9e4bdc7b05ae3048b7a47b7f1bb26af71c1cf7fd
Antonina Bondar <tonya.bondar111@gmail.com>
2022-06-25 22:25:50 +0000

    change render

--- src/index.js
+++ src/index.js
@@ -20,11 +20,9 @@ const loadmorebtn = new LoadMoreBtn({selector:'[type=button]',
 const newsPixabayService = new NewsPixabayService()
 console.log(newsPixabayService)
 
-console.log(newsPixabayService.per_page)
 let perpage = newsPixabayService.per_page
 let page = newsPixabayService.page
-console.log(page)
-console.log(perpage)
+
 
 
 form.addEventListener('submit', formSubmit)
@@ -32,37 +30,34 @@ loadmorebtn.refs.button.addEventListener('click', onLoadMore)
 
 function formSubmit(event){
   event.preventDefault()
-
-  clearGallery()
   newsPixabayService.query = event.target.elements.searchQuery.value
-  loadmorebtn.show()
   newsPixabayService.resetPage()
   if(newsPixabayService.query === ''){
-    loadmorebtn.hide()
    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
    return
   }
-getPixabay()
-
+getPixabayApi()
+clearGallery()
 }
 
-async function getPixabay(){
-loadmorebtn.disable()
+async function getPixabayApi(){
 try {
     const response = await newsPixabayService.getPixabay()
     const {hits,totalHits} = response.data
     console.log(response)
-    loadmorebtn.enable()
-    renderPixabay(hits)
+    gallery.insertAdjacentHTML('beforeend', renderPixabay(hits))
+    loadmorebtn.show()
+    page+=1
      perpage = Math.ceil(totalHits/40)
      console.log(perpage)
-    if( newsPixabayService.page > perpage ){
+    if( newsPixabayService.page > perpage && hits.length !== 0){
       loadmorebtn.hide()
       Notify.info("We're sorry, but you've reached the end of search results.")
     }
     if(hits.length === 0){
-      loadmorebtn.hide()
       Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      loadmorebtn.hide()
+      return
     }
     
     if(hits.length !== 0){
@@ -77,41 +72,54 @@ catch(error){
 }
 
 function onLoadMore(){
-getPixabay()
+getPixabayApi()
 }
 
 
 function renderPixabay( hits){
-const murkup = hits.map(card =>{
-  return `
-  <div class='photo-card'>
-  <a href='${card.largeImageURL}'><img
-      src='${card.webformatURL}'
-      alt='${card.tags}'
-      loading='lazy'
-    /></a>
-  <div class='info'>
+ return hits.map(
+  ({
+    webformatURL,
+    largeImageURL,
+    likes,
+    views,
+    comments,
+    tags,
+    downloads,
+  }) => {
+    return `<div class='photo-card'>
+<a href='${largeImageURL}'><img
+    src='${webformatURL}'
+    alt='${tags}'
+    loading='lazy'
+  /></a>
+<div class='info'>
   <p class='info-item'>
-  <b><span class= 'span'>likes:</span> ${card.likes}</b>
+    <b>Likes</b>
+    ${likes}
   </p>
-  <p class="info-item">
-    <b>views: ${card.views}</b>
+  <p class='info-item'>
+    <b>Views</b>
+    ${views}
   </p>
-  <p class="info-item">
-    <b>comments: ${card.comments}</b>
+  <p class='info-item'>
+    <b>Comments</b>
+    ${comments}
   </p>
-  <p class="info-item">
-    <b>downloads: ${card.downloads}</b>
+  <p class='info-item'>
+    <b>Downloads</b>
+    ${downloads}
   </p>
-  </div>
-  </div>
-  `
-}).join('')
-gallery.innerHTML = murkup
+</div>
+</div>`;
+  }
+)
+.join('');
 }
 
 function clearGallery(){
-gallery.innerHTML = ''
+  gallery.innerHTML =''
+
 }
 
 function lightbox(){
